40a9ff7672dc33676b88bac6a0e5e627d68383e3
Renmauzuo <renmauzuo@yahoo.com>
2021-09-04 19:39:30 -0500

    Initial Commit.
    Initial commit of clan party management. Enables interacting with the inventory of companions who are in the same army or settlement.

---
+++ ClanPartyManagement/ClanPartyManagementPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.Core.ViewModelCollection;
+
+namespace ClanPartyManagement
+{
+    [HarmonyPatch(typeof(SPInventoryVM), "RefreshValues")]
+    class ClanPartyManagementPatch
+    {
+        public static void Prefix(ref SPInventoryVM __instance)
+        {
+            TroopRoster newRoster = TroopRoster.CreateDummyTroopRoster();
+            newRoster.Add(__instance.TroopRoster);
+            foreach (Hero hero in Clan.PlayerClan.Heroes)
+            {
+                if (hero.IsAlive && !hero.IsChild && !newRoster.Contains(hero.CharacterObject) && ((hero.CurrentSettlement != null && hero.CurrentSettlement == Hero.MainHero.CurrentSettlement) || (hero.PartyBelongedTo != null && hero.PartyBelongedTo.Army != null && hero.PartyBelongedTo.Army == MobileParty.MainParty.Army)))
+                {
+                    __instance.CharacterList.AddItem(new SelectorItemVM(hero.Name));
+                    newRoster.AddToCounts(hero.CharacterObject, 1);
+                }
+            }
+            __instance.TroopRoster = newRoster;
+        }
+    }
+}
